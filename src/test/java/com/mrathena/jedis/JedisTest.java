0a78b742fc1f5149c28134ede74ab4bc8839d3c2
mrathena <admin@mrathena.com>
2019-08-11 17:02:26 +0800

    .

--- src/test/java/com/mrathena/jedis/JedisTest.java
+++ src/test/java/com/mrathena/jedis/JedisTest.java
@@ -15,11 +15,6 @@ public class JedisTest {
 	@Before
 	public void before() {
 		this.jedis = Common.getJedis();
-		jedis.set("string", "value");
-		jedis.rpush("list", "value");
-		jedis.sadd("set", "value");
-		jedis.hset("hash", "key", "value");
-		jedis.zadd("zset", 0.1, "value");
 	}
 
 	@After
@@ -31,18 +26,45 @@ public class JedisTest {
 	}
 
 	@Test
-	public void expire() {
-		// EXPIRE
-		System.out.println(jedis.exists("string"));
-		System.out.println(jedis.exists("string", "key2"));
-		System.out.println(jedis.exists("list"));
-		System.out.println(jedis.exists("set"));
-		System.out.println(jedis.exists("hash"));
-		System.out.println(jedis.exists("zset"));
+	public void expire() throws Exception {
+		// EXPIRE key seconds
+		// 设置key的过期时间，超过时间后，将会自动删除该key。在Redis的术语中一个key的相关超时是不确定的。
+		// 过期时间可能会被del,set,getset等操作清楚掉
+		System.out.println(jedis.set("key", "value"));
+		System.out.println(jedis.expire("key", 1));
+		Common.print(jedis);
+		Thread.sleep(1000L);
+		Common.print(jedis);
+		System.out.println(jedis.set("key", "value"));
+		System.out.println(jedis.expire("key", 1));
+		Thread.sleep(500L);
+		System.out.println(jedis.set("key", "newValue"));
+		Thread.sleep(1000L);
+		Common.print(jedis);
+	}
+
+	@Test
+	public void persist() throws Exception {
+		// PERSIST key
+		// 移除给定key的生存时间，将这个 key 从『易失的』(带生存时间 key )转换成『持久的』(一个不带生存时间、永不过期的 key )。
+		System.out.println(jedis.set("key", "value"));
+		System.out.println(jedis.expire("key", 1));
+		System.out.println(jedis.persist("key"));
+		Thread.sleep(1500L);
+		Common.print(jedis);
 	}
 
 	@Test
 	public void exist() {
+		// EXISTS key [key ...]
+		// 返回key是否存在。
+		// Since Redis 3.0.3 it is possible to specify multiple keys instead of a single one.
+		// In such a case, it returns the total number of keys existing
+		jedis.set("string", "value");
+		jedis.rpush("list", "value");
+		jedis.sadd("set", "value");
+		jedis.hset("hash", "key", "value");
+		jedis.zadd("zset", 0.1, "value");
 		System.out.println(jedis.exists("string"));
 		System.out.println(jedis.exists("string", "key2"));
 		System.out.println(jedis.exists("list"));
@@ -53,7 +75,14 @@ public class JedisTest {
 
 	@Test
 	public void type() {
+		// TYPE key
+		// 返回key所存储的value的数据结构类型，它可以返回string, list, set, zset 和 hash等不同的类型。
 		// none(key不存在),string(字符串),list(列表),set(集合),zset(有序集),hash(哈希表)
+		jedis.set("string", "value");
+		jedis.rpush("list", "value");
+		jedis.sadd("set", "value");
+		jedis.hset("hash", "key", "value");
+		jedis.zadd("zset", 0.1, "value");
 		System.out.println(jedis.type("unknown"));
 		System.out.println(jedis.type("string"));
 		System.out.println(jedis.type("list"));
@@ -64,6 +93,8 @@ public class JedisTest {
 
 	@Test
 	public void del() {
+		// DEL key [key ...]
+		// 删除指定的一批keys，如果删除中的某些key不存在，则直接忽略。
 		Common.print(jedis);
 		jedis.del("string");
 		Common.print(jedis);
@@ -73,7 +104,8 @@ public class JedisTest {
 
 	@Test
 	public void flushDB() {
-		// 刷新当前DB
+		// FLUSHDB
+		// 删除当前数据库里面的所有数据。这个命令永远不会出现失败。这个操作的时间复杂度是O(N),N是当前数据库的keys数量。
 		jedis.select(14);
 		jedis.set("key", "value");
 		Common.print(jedis);
@@ -89,7 +121,8 @@ public class JedisTest {
 
 	@Test
 	public void flushAll() {
-		// 刷新所有DB,谨慎操作
+		// FLUSHALL
+		// 删除所有数据库里面的所有数据，注意不是当前数据库，而是所有数据库。这个命令永远不会出现失败。这个操作的时间复杂度是O(N),N是数据库的数量。
 		jedis.select(14);
 		jedis.set("key", "value");
 		Common.print(jedis);
